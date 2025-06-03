099a3f18671d9f0a5f39b4573ca5db70945fe640
DanilaDenisov1407 <danila.denisov.work@gmail.com>
2025-06-03 13:39:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { showSlotMachine } from './slot.js';
 import { showPackOpen } from './pack.js';
 
 export let images = [];
-export let balance = { coins: 100, gold: 50, tokens: 10 };
+export let balance = { coins: 100, gold: 100, tokens: 100 };
 
 export function updateBalanceUI() {
   document.getElementById('coins').textContent = balance.coins;
@@ -11,9 +11,9 @@ export function updateBalanceUI() {
 }
 
 window.addFunds = function () {
-  balance.coins += 2000;
-  balance.gold += 2000;
-  balance.tokens += 2000;
+  balance.coins += 1000;
+  balance.gold += 1000;
+  balance.tokens += 1000;
   updateBalanceUI();
 };
 
@@ -21,11 +21,14 @@ fetch('data/cards.json')
   .then(res => res.json())
   .then(data => {
     const cards = data.success?.cards || [];
+
     cards.forEach(card => {
+      const name = card.name;
+      const sketcher = card.sketcher || '';
       (card.variants || []).forEach(v => {
         const url = v.art;
         if (url && url.includes('http') && !url.includes('None') && !url.includes('Not Available')) {
-          images.push({ name: card.name, image: url });
+          images.push({ name, sketcher, image: url });
         }
       });
     });
