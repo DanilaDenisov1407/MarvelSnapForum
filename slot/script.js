b6b677b3a22c8e4f7eff5c4fdfd69b82da559bd3
DanilaDenisov1407 <danila.denisov.work@gmail.com>
2025-11-17 21:28:37 +0100

    Update script.js

--- slot/script.js
+++ slot/script.js
@@ -182,10 +182,10 @@ function spin() {
     stopIndices = [];
    
     // Rigged шансы
-    if (Math.random() < 1 / 100) { // Джекпот ~1 к 100
+    if (Math.random() < 1 / 10) { // Джекпот ~1 к 100
         const jackpotIndex = Math.floor(Math.random() * baseSymbols.length);
         stopIndices = [jackpotIndex, jackpotIndex, jackpotIndex];
-    } else if (Math.random() < 0.5) { // Победа ~50%
+    } else if (Math.random() < 0.005) { // Победа ~50%
         // Найти персонажа с >=3 вариантами (specialVariants)
         const specialUrls = baseSymbols.filter(url => getCharacterId(url) === getCharacterId(baseSymbols[baseSymbols.length - 1])); // Предполагаем special в конце
         if (specialUrls.length >= 3) {
